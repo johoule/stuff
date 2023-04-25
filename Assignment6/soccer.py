1fe95c6f38fc97496ba2240a1ecdcf10de001a3f
ythuang13 <ythuang13@gmail.com>
2023-04-25 16:33:30 -0700

    Implement __init__

--- Assignment6/soccer.py
+++ Assignment6/soccer.py
@@ -49,4 +49,40 @@ class Soccer():
 
     # Game loop
     done: bool = False
+
+    def __init__(self, stars_count=200, clouds_count=20, darkness_offset=0):
+        """Soccer pygame constructor.
+        Optional Argument stars_count and clouds_count
+        for number of objects on screen
+        darkness offset changes how dark dark is, positive will make it darker,
+        negative will make it lighter
+        """
+        # config
+        pygame.display.set_caption(self.TITLE)
+
+        alpha = 200 + darkness_offset
+        if alpha > 255:
+            alpha = 255
+        elif alpha < 0:
+            alpha = 0
+        self.DARKNESS.set_alpha(alpha)
+        self.DARKNESS.fill((0, 0, 0))
+
+        self.SEE_THROUGH.set_alpha(150)
+        self.SEE_THROUGH.fill((124, 118, 135))
+
+        # stars and cloud random placement
+        self.STARS_COUNT = stars_count
+        self.CLOUDS_COUNT = clouds_count
+        for _ in range(self.STARS_COUNT):
+            x = random.randrange(0, 800)
+            y = random.randrange(0, 200)
+            r = random.randrange(1, 2)
+            self.stars.append([x, y, r, r])
+
+
+        for _ in range(self.CLOUDS_COUNT):
+            x = random.randrange(-100, 1600)
+            y = random.randrange(0, 150)
+            self.clouds.append([x, y])
     
