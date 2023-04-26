ff0ceeda9d8f2e86b8ddcc6171333af47a2ad426
zackgeek <131562039+zackgeek@users.noreply.github.com>
2023-04-25 22:46:52 -0700

    Update graphics_v4.py

--- Intro to Pygame Graphics/major league soccer animation/graphics_v4.py
+++ Intro to Pygame Graphics/major league soccer animation/graphics_v4.py
@@ -1,3 +1,4 @@
+
 # Imports
 import pygame
 import math
@@ -53,13 +54,11 @@ def draw_cloud(color, x, y, scale):
     The cloud shape consists of four ellipses and one rectangle, all with the same color specified by the 
     global variable cloud_color. The coordinates of the ellipses and rectangle are calculated based on the
     given x and y coordinates.
-
     Parameters:
     color (tuple): The color for the cloud as a tuple of 3 integers representing the RGB color.
     x (int): The x-coordinate of the top-left corner of the cloud shape.
     y (int): The y-coordinate of the top-left corner of the cloud shape.
     scale (float): Scale factor to resize the cloud.
-
     Returns:
     None
     '''
@@ -80,14 +79,12 @@ def drawGrass(color1, color2):
 def stadium_light(pole_color, pole_dimensions, lights_color, light_dimension_x, light_dimension_y ):
         '''stadium_light function creates the stadium lights that include the pole and 10 lights. The parameters allow the change of
         colors and positioning of the pole and lights.
-
         Parameters:
         pole_color (String):            desired color for the pole
         pole_dimensions (int List):     dimensions of the pole. [left, top, width, height]
         lights_color (String):          desired color for the lights
         light_dimension_x:              where the desired rows of lights would start being drawn from
         light_dimension_y:              where the desired rows of lights would end at
-
         Returns:
         None        
         '''
@@ -117,7 +114,6 @@ def fence(color):
         color (String):     desired color of the fence
         x_pos (int):        x position of the fence
         y_pos (int):        y position of the fence
-
         Returns:
         None
         '''
@@ -170,6 +166,8 @@ def netPartOne(color, width):
     pygame.draw.line(screen, color, [465, 140], [453, 200], width)
     pygame.draw.line(screen, color, [470, 140], [456, 200], width)
     pygame.draw.line(screen, color, [475, 140], [459, 200], width)
+    '''pygame draw line function are commands used to draw the net. The color and width specified in the function
+    parameter are applied to each line drawing. each line beginning and end points coordinates'''
 
 def netPartTwo(color, width):
         
@@ -182,7 +180,13 @@ def netPartTwo(color, width):
             pygame.draw.line(screen, color, [StartingXAxis, StartingYAxis], [endingXAxis, endingYAxis], width)
             endingXAxis += 2
             endingYAxis -= 2
+'''uses a for loop to draw a series of diagonal lines with a constant slope. The loop iterates 8 times, and on each iteration
+ a new diagonal line is drawn using pygame.draw.line.
 
+ 
+StartingXAxis, StartingYAxis, endingXAxis, and endingYAxis variables. These variables are initialized before the loop, and their values are updated on each iteration to create the diagonal slope.
+variables are updated on each iteration of the loop using the += operator to increase the X.
+'''
 
 def netPartThree(color, width):
 
@@ -194,9 +198,11 @@ def netPartThree(color, width):
     for i in range(8):
         pygame.draw.line(screen, color, [StartingXAxis, StartingYAxis], [endingXAxis, endingYAxis], width)
         endingXAxis -= 2
-        endingYAxis -= 2         
+        endingYAxis -= 2     
+    '''same as above expect in the loop using the -= operator to decrease the x that creates a diagonal slope with a constant slope of +1'''    
 
 def netPartFour(color, width):
+
     #net part 4
     pygame.draw.line(screen, color, [324, 144], [476, 144], width)
     pygame.draw.line(screen, color, [324, 148], [476, 148], width)
@@ -212,7 +218,11 @@ def netPartFour(color, width):
     pygame.draw.line(screen, color, [335, 188], [465, 188], width)
     pygame.draw.line(screen, color, [335, 192], [465, 192], width)
     pygame.draw.line(screen, color, [335, 196], [465, 196], width)
-
+    '''netPartFour uses a combination of horizontal and diagonal lines to create the net shape. The first 9 lines are drawn horizontally using pygame.draw.line
+    to create the top of the net, while the remaining 5 lines are drawn diagonally to create the bottom part of the net.
+    
+    a combination of horizontal and diagonal lines to create the overall shape'''
+    
 def goalPostBars(color):
     #draw bars for goal post
     pygame.draw.rect(screen, color, [320, 140, 160, 80], 5)
@@ -222,12 +232,14 @@ def goalPostBars(color):
     pygame.draw.line(screen, color, [320, 140], [340, 200], 3)
     pygame.draw.line(screen, color, [480, 140], [460, 200], 3)
 
+
 def drawGoalPost(color, width):
     netPartOne(color,width)
     netPartTwo(color, width)
     netPartThree(color,width)
     netPartFour(color, width)
     goalPostBars(color)
+    '''this function calls and all parts of the net to complete the shape of the goal post.'''
 
 def drawStands (upperStandColor, lowerStandColor):
     #stands right
@@ -237,6 +249,11 @@ def drawStands (upperStandColor, lowerStandColor):
     #stands left
     pygame.draw.polygon(screen, lowerStandColor, [[120, 220], [0, 340], [0, 290], [120, 180]])
     pygame.draw.polygon(screen, upperStandColor, [[120, 180], [0, 100], [0, 290]])
+    '''Each set of stands is drawn using pygame.draw.polygon, which allows for the drawing of irregular shapes with multiple vertices.
+    The lowerStandColor argument is used to color the lower parts of the stands, while the upperStandColor argument is used to color the upper parts of the stands.
+    
+    The vertices of each polygon are hardcoded into the function using a series of [x, y] coordinate pairs. 
+    The first pair in each polygon corresponds to the bottom left corner of the stand, and the subsequent pairs define the shape of the polygon.'''
 
 def drawLeftFlag(x, y, poleColor, flagColor):
     pygame.draw.line(screen, poleColor, [x, y], [x-5, y-30], 3)
@@ -245,6 +262,8 @@ def drawLeftFlag(x, y, poleColor, flagColor):
 def drawRightFlag(x, y, poleColor, flagColor):
     pygame.draw.line(screen, poleColor, [x, y], [x+5, y-30], 3)
     pygame.draw.polygon(screen, flagColor, [[x+8, y-30], [x+15, y-24], [x+5, y-15]]) 
+    '''The vertices of the polygon are hardcoded into the function using a series of [x, y] coordinate pairs. 
+    The coordinates of these pairs determine the shape of the flag.'''
 
 def drawFieldLines(color):
     #bottom side of out of bounds
@@ -259,7 +278,8 @@ def drawFieldLines(color):
 
     #safety circle
     pygame.draw.ellipse(screen, color, [240, 500, 320, 160], 5)
-
+    '''The function begins by drawing a line along the bottom edge of the screen to mark the out-of-bounds area. 
+    It then draws lines along the left and right edges of the field to define the out-of-bounds areas on those sides. Next, it draws an ellipse to mark the "safety circle" in the center of the field.'''
     #18 yard line goal box
     pygame.draw.line(screen, color, [260, 220], [180, 300], 5)
     pygame.draw.line(screen, color, [180, 300], [620, 300], 3)
@@ -272,13 +292,15 @@ def drawFieldLines(color):
 
     #arc at the top of the goal box
     pygame.draw.arc(screen, color, [330, 280, 140, 40], math.pi, 2 * math.pi, 5)
+    '''The function then draws the 18-yard line goal box, which is the rectangular area in front of each goal where certain rules apply. 
+    Finally, it draws the smaller 6-yard line goal box within the larger box, and the curved arc at the top of the larger box that defines the penalty area'''
     
 def drawStar(x, y, color, scale):
     pygame.draw.ellipse(screen, color, [x,y,scale,scale])
-
+    '''The size of the ellipse is determined by the scale argument specifies the width and height of the ellipse in pixels'''
 # Config
-lights_on = True
-day = True
+lights_on = True #This variable could be used to control whether or not the lights in a game or application are turned on or off.
+day = True #This variable could be used to control the appearance or behavior of the game or application based on the time of day.
 
 stars = []
 for n in range(200):
@@ -288,6 +310,11 @@ for n in range(200):
     color = random.choice([RED,GREEN,BLUE,YELLOW,WHITE])
     scale = random.uniform(0.5, 3)
     stars.append([x, y, color, scale])
+    '''for loop to generate 200 stars 
+    x,y are set randomly generates color and scale also has a certain range 
+    r is the radius of the star 
+    color is also randomly generated from 5 different colors
+    scale is also randomly generated size between 0.5 to 3 f'''
 
 clouds = []
 for i in range(20):
@@ -295,10 +322,14 @@ for i in range(20):
     y = random.randrange(0, 150)
     scale = random.uniform(0.5, 3)
     clouds.append([x, y, scale])
-    
+    '''for loop generate 20 clouds 
+    x,y make a random integer between two values for the size of the cloud 
+    scale is also a random floating point for the size of each cloud 
+    '''
+
 # Game loop
 done = False
-
+'''the done variable can be set to True to stop the game loop and exit the program.'''
 
 # Counter to allow flags to bounce up and down
 flagVerticalCounter = 0
@@ -355,7 +386,7 @@ while not done:
 
 
     '''fence'''
-    fence(NIGHT_GRAY)
+    fence(NIGHT_GRAY) 
 
     if day:
         pygame.draw.ellipse(screen, BRIGHT_YELLOW, [520, 50, 40, 40])
@@ -365,8 +396,8 @@ while not done:
 
     for c in clouds:
         draw_cloud(cloud_color, c[0], c[1], c[2])
-    screen.blit(SEE_THROUGH, (0, 0))   
-    
+    screen.blit(SEE_THROUGH, (0, 0)) #transparency 
+    '''to draw the fence set the color for night and day and loop that iterates over the colouds'''
 
     drawFieldLines(WHITE)
     
@@ -376,6 +407,7 @@ while not done:
     #score board
     pygame.draw.rect(screen, BLACK, [300, 40, 200, 90])
     pygame.draw.rect(screen, WHITE, [301, 41, 198, 88], 2)
+    
 
 
     #light pole and lights 1
@@ -384,9 +416,8 @@ while not done:
     #light pole and lights 2
     stadium_light(GRAY,[630, 60, 20, 140], light_color, 590,690)
 
-    #Function to draw the goal post with parameters Colour and width for net lines
+    #Function to draw the goal post with parameters Color and width for net lines
     drawGoalPost(WHITE,1)
-
     drawStands(WHITE, RED)
 
 
